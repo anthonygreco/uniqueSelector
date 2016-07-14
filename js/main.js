89914dac878676134a7cdc8b896abaa03e920f33
Anthony Greco <anthony.greco@gmail.com>
2016-07-14 14:57:15 -0400

    demo updated

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(function() {
+  alert('Click any element to log its unique selector string to the console');
   $('*').on({
     click: function(event) {
       event.stopPropagation();
       var uniqueSelector = $(event.target).uniqueSelector();
+      console.log('uniqueSelector:', uniqueSelector);
       $(uniqueSelector).fadeOut('fast', function() {
         $(uniqueSelector).fadeIn('fast');
       });
